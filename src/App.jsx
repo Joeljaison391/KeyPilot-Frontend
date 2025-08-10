0154bc36e52841824360aa422084b18e6d3eeba5
Abelboby <abeldavisboby@gmail.com>
2025-08-10 19:37:56 +0530

    feat: Add @vercel/analytics integration for enhanced application analytics tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
+import { Analytics } from '@vercel/analytics/react'
 import { AuthProvider } from './context/AuthContext'
 import { ConsentProvider } from './context/ConsentContext'
 import { TourProvider } from './context/TourContext'
@@ -91,6 +92,7 @@ function App() {
       <TourProvider>
         <ApiKeyTutorialProvider>
           <AppContent />
+          <Analytics />
         </ApiKeyTutorialProvider>
       </TourProvider>
     </ConsentProvider>
